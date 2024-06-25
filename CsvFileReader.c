4eda3365edf5f6bf611bbeac13e3b99f23adf05c
Amal C <amalc.plr@gmail.com>
2024-06-25 14:49:55 +0530

    used delimiter

--- CsvFileReader.c
+++ CsvFileReader.c
@@ -1,32 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void){
+int main(void) {
     char buffer[1000];
     char *data;
+    
     // Open csv file in read mode, returns null if cant open
-    FILE *csvFile = fopen("wrongCsvTest.csv", "r");
+    FILE *csvFile = fopen("sampleCsv.csv", "r");
 
-    if(csvFile == NULL){
-        printf("ERROR: cant open file \n");
-        // terminate pgm with err status
+    if (csvFile == NULL) {
+        printf("ERROR: Cannot open file\n");
+          // terminate pgm with err status
         exit(-1); 
     }
 
-    printf("Opening the file\n");
+    printf("Parsing the CSV file\n");
     printf("-----------------------------\n");
 
-     // Read and print each line in the file
-    while(fgets(buffer, sizeof(buffer), csvFile)){
-        printf("%s\n", buffer);
+   // Read and print each line in the file
+    while (fgets(buffer, sizeof(buffer), csvFile)) {
+        // strtok to split each line into fields using , as delimiter
+        data = strtok(buffer, ",");
+        while (data != NULL) {
+            // Print each field
+            printf(" %s ", data);
+            // Move to the next field
+            data = strtok(NULL, ",");
+        }
     }
 
-
-
-    //close the csv file
+    // Close the CSV file
     fclose(csvFile);
     printf("\n-----------------------------\n");
+
     return 0;
 }
-    
